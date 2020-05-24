6dc2491926732d673f6178a73ba90cbae8374a8c
Jukka Rahkonen <jukka.rahkonen@iki.fi>
2020-05-25 00:08:57 +0300

    added assoc nic - nsg

--- add/networks.tf
+++ add/networks.tf
@@ -61,4 +61,9 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic[0].id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 # end networks
